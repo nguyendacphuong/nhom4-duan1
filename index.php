ba3a1d22ed47df9fe27c924ea87add2703af9e92
dactrong <trongndph13723@fpt.edu.vn>
2021-11-18 14:12:54 +0700

    update sql

--- index.php
+++ index.php
@@ -5,20 +5,11 @@ require_once './commons/utils.php';
 switch ($url) {
     case '/':
         require_once './client/business/homepage.php';
-        home();
+        
         break;
-    case 'gioi-thieu':
+    case 'dangki':
         require_once './client/business/homepage.php';
-        about();
-    case 'danh-muc':
-        require_once './client/business/category.php';
-        list_product();
-        break;
-    case 'cp-admin':
-        require_once './admin/business/dashboard.php';
-        dashboard_index();
-        break;
-
+        
     default:
         # code...
         break;
